db028054b327bef53471c518a3ac6d0cedcb5e28
PepaPalaxo <josef.stroleny@palaxo.com>
2019-07-17 15:55:19 +0200

    Updated fields

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # es-odm
+
+- Some notes, might change...
+
+### Fields
+
+#### Primitive fields
+
+ - NumberField, StringField, BooleanField, ...
+ - Usage: `new NumberField(data.number, ?Joi.number(...) );`
+ 
+#### Special fields
+
+ - ArrayField: `new ArrayField(data.array, { type: StringField }, ?Joi.array(...) );`
+ 
+ - NestedField: `new NestedField(data.object, { a: BooleanField, b: NumberField }, ?Joi.any(...) )`
+ 
+ 
+#### Usage example - nesting
+
+```
+const object = new NestedField(data.object, {
+    name: StringField,
+    id: NumberField,
+    phones: {
+        type: ArrayField, content: {
+            type: NestedField, content: {
+                mobile: BooleanField,
+                number: StringField
+            }
+        }
+    },
+    preferences: {
+        type: NestedField, content: {
+            language: StringField,
+            flags: {
+                type: ArrayField, content: {
+                    type: BooleanField
+                }
+            }
+        }
+    }
+}, Joi.any(...) );
+```
+ 
+ 
+### BaseModel
--- app.js
+++ app.js
@@ -5,48 +5,93 @@ const StringField = require(`./lib/fields/StringField`);
 const BooleanField = require(`./lib/fields/BooleanField`);
 const NumberField = require(`./lib/fields/NumberField`);
 const NestedField = require(`./lib/fields/NestedField`);
+const ArrayField = require(`./lib/fields/ArrayField`);
 
 //PlayGround
 class User extends BaseModel {
     constructor(data = {}) {
         super();
 
-        this.name = new StringField({ required: true }, data.name);
+        this.name = new StringField(data.name);
 
-        this.configuration = new NestedField({
-            x: new BooleanField(),
-            y: new NumberField(),
-            undef: new StringField({ required: true })
-        }, data.configuration);
+        this.configuration = new NestedField(
+            data.configuration,
+            {
+                x: BooleanField,
+                y: NumberField,
+                undef: StringField
+            }
+        );
+
+        this.object = new NestedField(data.object, {
+            name: StringField,
+            id: NumberField,
+            phones: {
+                type: ArrayField, content: {
+                    type: NestedField, content: {
+                        mobile: BooleanField,
+                        number: StringField
+                    }
+                }
+            },
+            preferences: {
+                type: NestedField, content: {
+                    language: StringField,
+                    flags: {
+                        type: ArrayField, content: {
+                            type: BooleanField
+                        }
+                    }
+                }
+            }
+        });
     }
 }
 
 const application = function () {
-    //??? validace - co, chyba vs false
-    //??? array ve Field
-    //??? zmena ve Field jen pres .value?
-    //??? ma undefined StringField vracet undefined nebo ''?
-    //??? field nezna sve jmeno, je to OK? (treba pri vyhozeni chyby)
+    //TODO ↓↓↓
+
+    //intercept ArrayField returned array -> changes backpropagated
+    //array.pop() not working
+
+    //rewrite [util.inspect.custom](depth, opts) { } for BaseModel, NestedField and ArrayField
+
+    //Simplify it
+
+    //Joi - validations not working
+
+    //use ES6
+    // - new library + ES6 -> don't send any type
+    // - type() same as in()
+    //
+    // - _id -> StringField in base class - undefined by default
 
     const user = new User({
         name: `unman`,
-        configuration: { x: true, y: 5 }
+        configuration: { x: true, y: 5 },
+        object: {
+            preferences: {
+                flags: [true, false, false, true]
+            }
+        }
     });
 
-    console.log(user.name); //StringField {}
-    console.log(user.name.value);   //unman
+    console.log(user);
 
-    console.log(user.configuration);    //NestedField {}
-    console.log(user.configuration.value);  //{ x: true, y: 5, undef: undefined }
+    //console.log(user.configuration.x);
+    //console.log(user.object.preferences.flags);
+    //console.log(user.object.preferences.flags[0]);
+    //console.log(user.object.preferences.flags[1]);
+    //console.log(user.object.preferences.flags[2]);
+    //console.log(user.object.preferences.flags[3]);
 
-    console.log(user.configuration.x);  //BooleanField {}
-    console.log(user.configuration.x.value);    //true
-
+    /*
     try {
-        user.configuration.validate();
+        user.validate();
     } catch (e) {
-        console.log(e.toString());  //Value is required!
+        console.log(e);
     }
+    */
 };
 
 module.exports = application();
